fcdd2c7d3d4c54ea70729fc8453f690ed5aa00ab
siimanlabs <siimanlabs.dev@gmail.com>
2026-01-23 22:13:17 -0800

    feat(ui): add card component

---
+++ frontend/src/app/shared/ui/card/card.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'ds-card'
+  }
+})
+export class CardComponent {}
